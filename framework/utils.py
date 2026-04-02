6469541f34a02a55e70eb4b8a2cbc72f8a8faa29
linxuxun <163323355+linxuxun@users.noreply.github.com>
2026-04-02 14:00:28 +0800

    feat: add framework/utils.py

---
+++ framework/utils.py
@@ -0,0 +1,9 @@
+"""框架工具函数"""
+from . import (
+    set_all_modules_log_files, get_all_modules_log_files,
+    get_module_log_files, set_root_path, get_root_path
+)
+__all__ = [
+    "set_all_modules_log_files", "get_all_modules_log_files",
+    "get_module_log_files", "set_root_path", "get_root_path"
+]
